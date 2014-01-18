b1a12e7a30bf8689dbe30686e11ab367bc75b891
Alex Kehayias <alex.kehayias@gmail.com>
2014-01-18 17:32:05 -0500

    Move all state to it's own namespace

--- src/cljs/engine/core.cljs
+++ src/cljs/engine/core.cljs
@@ -7,26 +7,12 @@
             ;; Imports the entity records
             ;; TODO can I explicitely import just the entity? or must
             ;; I mark it with :as?
-            [chocolatier.engine.entities :as e])
+            [chocolatier.engine.entities :as e]
+            [chocolatier.engine.state :as s])
   (:use-macros [dommy.macros :only [node sel sel1]]))
 
 
-;; TODO need a defonce so we can eval theo whole namespace more than once
-;; Global game state
-(def game (atom nil))
 
-;; System example:
-;; Render
-;; (when (satisfies? obj renderable)
-;;       (draw obj))
-(def systems (atom []))
-
-;; TODO entities should be a hashmap for easy removal from the game
-;; state without having to iterate through the whole list
-(def entities (atom []))
-(def paused? (atom false))
-
-;; TODO a function to inspect/capture all of the current state
 ;; TODO a function that takes a state hashmap and starts the game from it
 
 (defn create-entity!
@@ -40,7 +26,7 @@
     (set! (.-position.y (:sprite bunny)) pos-y)
     (set! (.-anchor.x (:sprite bunny)) anc-x)
     (set! (.-anchor.y (:sprite bunny)) anc-y)
-    (swap! entities conj bunny)))
+    (swap! s/entities conj bunny)))
 
 (defn draw
   "Renders all the things to the screen.
@@ -48,8 +34,8 @@
    Iterates through all the entities and renders them to the stage.
    Renders the stage to the screen"
   [renderer stage]
-  (when-not @paused?
-    (doseq [e @entities]
+  (when-not (:paused @s/game)
+    (doseq [e @s/entities]
           (c/render e stage))
     (.render renderer stage)))
 
@@ -57,7 +43,7 @@
   "Tick the game by miliseconds of time"
   []
   ;; Update the global entities state atom
-  (doseq [e @entities]
+  (doseq [e @s/entities]
     (c/tick e)))
 
 ;; TODO combine the game-loop and render-loop as systems that
@@ -67,7 +53,7 @@
   "Calculate changes based on the time since last change"
   [renderer stage]
   ;; TODO Calculate the changes since the last game tick
-  (when-not @paused?
+  (when-not (:paused @s/game)
     (tick-game)
     (draw renderer stage)))
 
@@ -82,8 +68,7 @@
   "Renders the game every n seconds.
    Hashmap of game properties."
   []
-  (let [;; TODO get the screen height and width
-        ;; TODO allow the screen height and width to be variable?
+  (let [;; TODO reset the game height on screen resize
         width (aget js/window "innerWidth")
         height (aget js/window "innerHeight")
         frame-rate 60
@@ -94,25 +79,28 @@
     ;; TODO return a function that also cleans up the canvas 
     {:renderer renderer
      :stage stage
-     :end main-loop}))
+     :end main-loop
+     :paused false}))
 
 
 ;; Start, stop, reset the game. Game is stored in an atom and
 ;; referenced directly in these functions
 (defn start-game! []
-  (reset! game (init-game)))
+  (reset! s/game (init-game)))
 
 (defn stop-game!
   "Stop the game loop and remove the canvas"
   []
-  (when-not (empty? @game)
+  (when-not (empty? @s/game)
     (dom/remove! (sel1 :canvas))
-    (:end @game)))
+    (:end @s/game)))
 
 (defn reset-game!
   "End the"
   []
   (stop-game!)
-  (reset! entities [])
+  (reset! s/entities [])
   (start-game!)
-  (create-entity! (:stage @game) "static/images/bunny.png" 500 500 0.05 0.05))
+  (create-entity! (:stage @s/game)
+                  "static/images/bunny.png"
+                  500 500 0.05 0.05))
---
+++ src/cljs/engine/state.cljs
@@ -0,0 +1,23 @@
+(ns chocolatier.engine.state
+  (:require-macros [chocolatier.macros :refer [defonce]]))
+
+
+;; TODO a function to inspect/capture all of the current state that
+;; can be read in to a game to start it
+
+;; Global game state
+(defonce game (atom nil))
+
+;; TODO Should entities be a hashmap for easy removal from the game
+;; state without having to iterate through the whole list? Or will we
+;; need to be filtering the whole list any way
+(defonce entities (atom []))
+
+(defonce systems (atom {}))
+
+(defn get-state
+  "Return all objects in the game"
+  []
+  {:entities entitities
+   :systems? systems
+   :game game})
