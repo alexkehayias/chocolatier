341acede2184a35abd14bca3cb88bd66b8951155
Alex Kehayias <alex.kehayias@gmail.com>
2015-12-13 18:31:22 -0500

    Forgot to add counters namespace

---
+++ src/cljs/chocolatier/engine/utils/counters.cljs
@@ -0,0 +1,22 @@
+(ns chocolatier.engine.utils.counters)
+
+
+(defn mk-cooldown [limit]
+  {:counter 0 :limit limit})
+
+(defn cooldown?
+  "Takes a cooldown hashmap and returns a boolean if it is in cooldown"
+  [{:keys [counter limit]}]
+  (when (and counter limit)
+    (zero? counter)))
+
+(defn tick-cooldown
+  "Increments counter of the cooldown and returns a pair of updated
+   cooldown and boolean of whether it's still in cooldown. Resets
+   counter to 0 if cooldown counter exceeds the limit. Cooldown is not
+   in progress only if the counter is 0."
+  [{:keys [counter limit] :as cooldown}]
+  (cond
+    (zero? counter) [(update cooldown :counter inc) false]
+    (> (inc counter) limit) [(assoc cooldown :counter 0) true]
+    :else [(update cooldown :counter inc) true]))
